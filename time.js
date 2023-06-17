55c983f550d1197b793765e244351416f570c3b0
AICodeBase <136905624+AICodeBase@users.noreply.github.com>
2023-06-18 00:03:23 +0800

    Add files via upload

---
+++ time.js
@@ -0,0 +1,23 @@
+function updateTime() {
+  var currentTime = new Date();
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
+
+  // Add leading zero if needed
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  // Update time display
+  document.getElementById("current-time").innerHTML = hours + ":" + minutes + ":" + seconds;
+}
+
+// Update time every second
+setInterval(updateTime, 1000);
